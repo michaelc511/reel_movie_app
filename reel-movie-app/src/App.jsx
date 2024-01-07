b8a3c647bfca1deaf302002a0f35147eb40f115c
Jose <josemanuel21302@gmail.com>
2024-01-07 14:21:06 -0800

    added base Carousel with bootstrap

--- reel-movie-app/src/App.jsx
+++ reel-movie-app/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import useFetch from './tools/useFetch'
 import NavigationBar from './components/NavigationBar'
 import HomeCarousel from './components/HomeCarousel'
@@ -9,7 +8,6 @@ import HomeCarousel from './components/HomeCarousel'
 function App() {
   const { data, error } = useFetch()
 
-  console.log(data);
   return (
     <>
 
--- reel-movie-app/src/components/HomeCarousel.jsx
+++ reel-movie-app/src/components/HomeCarousel.jsx
@@ -1,10 +1,33 @@
+import Carousel from 'react-bootstrap/Carousel';
+import './/HomeCarousel.css'
+export default function HomeCarousel({ data }){
 
-
-export default function HomeCarousel(){
+  const getImageUrl = (posterPath) => {
+    const baseImageUrl = 'https://image.tmdb.org/t/p/w500';
+    return baseImageUrl + posterPath;
+  };
 
   return(
-    <>
-      this holds the carousel
-    </>
+    <div className="carouselContainer">
+      <Carousel className="custom-carousel">
+
+        {/* here I actually only want to iterate a few times so I will replace 'map' with a 'for loop'. */}
+        {data ? data.results.map((movie) => {
+
+          return(
+            <Carousel.Item key={movie.id}>
+              <img className="carouselImg" 
+                // text={movie.id}
+                src={getImageUrl(movie.poster_path)}
+                 />
+              <Carousel.Caption>
+                <h2>{movie.original_title}</h2>
+              </Carousel.Caption>
+            </Carousel.Item>
+          )
+
+        }) : 'Loading'}
+      </Carousel>
+    </div>
   )
 }
