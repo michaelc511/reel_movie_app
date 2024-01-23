b75c3e26b6d3d73894e7329f35d64f4acd38e42b
analuzac <analuzacevedo@gmail.com>
2024-01-22 16:53:37 -0800

    initial layout updates

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -5,24 +5,41 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [display, setDisplay] = useState('')
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        
+        
       </div>
       <h1>Vite + React</h1>
       <div className="card">
+      <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <p>
+          Movie description goes here
+        </p>
+        <button onClick={() => setDisplay((display) => display === '' ? 'Clicked view details button' : '')}>
+          Details & Review
+        </button>
+        <div>{display}</div>
+        
+      </div>
+      <div className="card">
+      <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        
+        <p>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+        <div>
+        Edit <code>src/App.tsx</code> and save to test HMR
+        </div>
+          
         </p>
       </div>
       <p className="read-the-docs">
